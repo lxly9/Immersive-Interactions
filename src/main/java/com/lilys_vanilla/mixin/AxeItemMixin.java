feac177c3e513c004a579e513271011fe69a34ea
Lily <lilyschlossnagel@icloud.com>
2025-03-12 15:08:48 +0100

    delete unused files, rewrote PatinaItem, changed some stuff

--- src/main/java/com/lilys_vanilla/mixin/AxeItemMixin.java
+++ src/main/java/com/lilys_vanilla/mixin/AxeItemMixin.java
@@ -2,11 +2,13 @@ package com.lilys_vanilla.mixin;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Oxidizable;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.AxeItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.block.Block;
 
@@ -19,19 +21,26 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(AxeItem.class)
 public class AxeItemMixin {
 
-    @Inject(at = @At("HEAD"), method = "useOnBlock")
+    @Inject(method = "useOnBlock", at = @At("HEAD"))
     private void injectScrape(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
         World world = context.getWorld();
         BlockPos pos = context.getBlockPos();
         BlockState state = world.getBlockState(pos);
+        PlayerEntity player = context.getPlayer();
 
-        if (!world.isClient && state.getBlock() instanceof Oxidizable oxidizable) {
+        if (!world.isClient && state.getBlock() instanceof Oxidizable && player != null) {
+            Oxidizable oxidizable = (Oxidizable) state.getBlock();
             Oxidizable.OxidationLevel oxidationLevel = oxidizable.getDegradationLevel();
 
             if (oxidationLevel.ordinal() > 0) {
-                Block.dropStack(world, pos, new ItemStack(ModItems.COPPER_PATINA));
+                if (!player.isCreative()) {
+                    Vec3d blockCenter = pos.toCenterPos();
+                    Vec3d playerPos = player.getPos();
+                    Vec3d direction = playerPos.subtract(blockCenter).normalize().multiply(0.55);
+                    Vec3d dropPos = blockCenter.add(direction);
+                    Block.dropStack(world, BlockPos.ofFloored(dropPos), new ItemStack(ModItems.COPPER_PATINA));
+                }
             }
         }
     }
 }
-
