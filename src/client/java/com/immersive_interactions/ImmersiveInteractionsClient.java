e1b03613639e05c3b2021fb2c80368f39434ae10
Lily <lilyschlossnagel@icloud.com>
2025-03-14 16:36:17 +0100

    renamed the mod, removed shears mixin

--- src/client/java/com/immersive_interactions/ImmersiveInteractionsClient.java
+++ src/client/java/com/immersive_interactions/ImmersiveInteractionsClient.java
@@ -2,7 +2,7 @@ package com.immersive_interactions;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class LilysVanillaClient implements ClientModInitializer {
+public class ImmersiveInteractionsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
