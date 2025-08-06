f2152394a85f0bdb0d4d2f89d65a853bf18d6ec5
Lily <lilyschlossnagel@icloud.com>
2025-08-06 19:50:13 +0200

    copper blockstates!!!!

--- src/main/java/com/immersive_interactions/ImmersiveInteractions.java
+++ src/main/java/com/immersive_interactions/ImmersiveInteractions.java
@@ -1,11 +1,17 @@
 package com.immersive_interactions;
 
 import com.immersive_interactions.item.ModItems;
+import com.immersive_interactions.mixin.BlockAccessor;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.OxidizableBlock;
+import net.minecraft.registry.Registries;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static com.immersive_interactions.util.ModProperties.*;
 
 
 public class ImmersiveInteractions implements ModInitializer {
@@ -17,11 +23,22 @@ public class ImmersiveInteractions implements ModInitializer {
 	public void onInitialize() {
 		LOGGER.info("Loaded Immersive Interactions");
 		ModItems.registerModItems();
-
-
+		setDefaultStatesAfterRegistration();
 	}
 
 	public static boolean isModLoaded(String modId) {
 		return FabricLoader.getInstance().isModLoaded(modId);
+	}
+
+	public void setDefaultStatesAfterRegistration() {
+		for (Block block : Registries.BLOCK) {
+			if (block instanceof OxidizableBlock) {
+				BlockState defaultState = block.getDefaultState();
+				if (!defaultState.getProperties().contains(DEGRADATION)) continue;
+
+				BlockState newDefault = defaultState.with(DEGRADATION, 0).with(WAXED, false);
+				((BlockAccessor) block).callSetDefaultState(newDefault);
+			}
+		}
 	}
 }
--- src/main/java/com/immersive_interactions/mixin/BlockMixin.java
+++ src/main/java/com/immersive_interactions/mixin/BlockMixin.java
@@ -2,10 +2,7 @@ package com.immersive_interactions.mixin;
 
 import net.minecraft.block.*;
 import net.minecraft.state.StateManager;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -20,14 +17,6 @@ public abstract class BlockMixin {
         if ((Object) this instanceof OxidizableBlock) {
             builder.add(DEGRADATION);
             builder.add(WAXED);
-        }
-    }
-
-    @Inject(method = "<init>", at = @At("TAIL"))
-    private void setDegradationDefaultState(AbstractBlock.Settings settings, CallbackInfo ci) {
-        if ((Object) this instanceof OxidizableBlock block) {
-            BlockState defaultState = block.getDefaultState().with(DEGRADATION, 0).with(WAXED, false);
-            ((BlockAccessor) block).callSetDefaultState(defaultState);
         }
     }
 }
