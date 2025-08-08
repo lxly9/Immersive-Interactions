faf7ebedb4ec76728043c23e14f6a99efa1fefd4
Lily <lilyschlossnagel@icloud.com>
2025-08-08 11:47:39 +0200

    new branch, new version

--- src/main/java/com/immersive_interactions/mixin/copper/compat/RailBlockMixin.java
+++ src/main/java/com/immersive_interactions/mixin/copper/compat/RailBlockMixin.java
@@ -1,18 +1,18 @@
-package com.immersive_interactions.mixin.copper;
+package com.immersive_interactions.mixin.copper.compat;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.BulbBlock;
-import net.minecraft.block.DoorBlock;
+import net.minecraft.block.RailBlock;
 import net.minecraft.state.StateManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(BulbBlock.class)
-public class BulbBlockMixin extends Block {
-    public BulbBlockMixin(Settings settings) {
+@Mixin(RailBlock.class)
+public class RailBlockMixin extends Block {
+
+    public RailBlockMixin(Settings settings) {
         super(settings);
     }
 
